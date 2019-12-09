cd84938e4e6a72e3b28970aa7e4d12208b6b184b
EismanW <pl.amosso@gmail.com>
2019-12-09 10:46:19 +0000

    added random function to wonder selection

--- client/src/components/WonderSelectionForm.vue
+++ client/src/components/WonderSelectionForm.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h2>Select your wonder</h2>
-        
+        <simple-button message="Get random wonder" eventChannel="select-random-wonder"></simple-button>
     </div>
 </template>
 
@@ -10,7 +10,25 @@ import { eventBus } from "../main";
 import SimpleButton from "./SimpleButton";
 
 export default {
-    name: "wonder-selection-form"
+    name: "wonder-selection-form",
+    props: {
+        wonders: Array
+    },
+    mounted() {
+        eventBus.$on("select-random-wonder", () => this.selectRandomWonder());
+    },
+    methods: {
+        selectRandomWonder: function() {
+            return this.getWonder(Math.random(0, wonders.length + 1));
+        },
+
+        getWonder: function(position) {
+            return this.wonders[position];
+        }
+    },
+    components: {
+        "simple-button": SimpleButton,
+    }
 }
 </script>
 
