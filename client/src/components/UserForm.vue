b994d92a9a6d442e9b98166c5f284009c7f09a28
Cameron <cameron.beckit@gmail.com>
2019-12-09 10:44:40 +0000

    adds eventBus to userForm

--- client/src/components/UserForm.vue
+++ client/src/components/UserForm.vue
@@ -1,17 +1,23 @@
 <template lang="html">
-  <form class="user-name">
+  <form class="user-name" v-on:submit.prevent="userFormSubmit">
       <label for="username">Name:</label>
-      <input id="username" type="text" v-model="name" name="username">
+      <input id="username" type="text"  name="username" v-model="username">
       <input id="submit" type="submit" name="" value="Submit">
   </form>
 </template>
 
 <script>
+import EventBus from '../main.js'
 export default {
   name: "user-form",
   data(){
     return{
-      name: ""
+      username: ""
+    }
+  },
+  methods:{
+    userFormSubmit: function (){
+      eventBus.$emit(username)
     }
   }
 }
