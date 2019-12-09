0f6dd983b4616b6cd9ee4f0d87ac407c1d1794d3
Yulirun Shao <yulirunshao@Yuliruns-MacBook-Pro.local>
2019-12-09 11:41:06 +0000

    done show route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -17,6 +17,18 @@ router.get('/', (req, res) => {
 });
 
 
+router.get('/:id',(req, res) => {
+  const id = req.params.id;
+  collection.findOne({ _id: ObjectId(id)});
+  .then((wonder) => res.json(wonder))
+  .catch((err) => {
+    console.error(err);
+    res.status(500);
+    res.json({ status: 500, error: err });
+  });
+});
 
+return router;
+};
 
-}
+module.exports = createRouter;
