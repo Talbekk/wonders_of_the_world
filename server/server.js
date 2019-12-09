fbf6d9947f3a50870228d7ea86729edf2855393e
Yulirun Shao <yulirunshao@Yuliruns-MacBook-Pro.local>
2019-12-09 11:12:34 +0000

    set up mongo client

--- server/server.js
+++ server/server.js
@@ -3,13 +3,19 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const createRouter = require('./helpers/createRouter.js');
+const MongoClient = require('mongodb').MongoClient;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const wonderRouter = createRouter;
-app.use('/api/wonders', wonderRouter);
-
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db('wonders_of_the_world');
+  const wondersCollection = db.collection('wonders');
+  const wonderRouter = createRouter(wondersCollection);
+  app.use('/api/wonders', wonderRouter);
+})
+.catch(console.err);
 
 app.listen(3000, function(){
   console.log(`app running on port ${this.address().port}`);
