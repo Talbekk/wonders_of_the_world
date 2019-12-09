5ca3e51fc0a7497a0f6fbfb2fb5a3d2e56ced877
EismanW <pl.amosso@gmail.com>
2019-12-09 11:08:01 +0000

    done wonder selection area

--- client/src/components/WonderSelectionForm.vue
+++ client/src/components/WonderSelectionForm.vue
@@ -2,12 +2,14 @@
     <div>
         <h2>Select your wonder</h2>
         <simple-button message="Get random wonder" eventChannel="select-random-wonder"></simple-button>
+        <simple-select message="Select a wonder" eventChannel="select-wonder"></simple-select>
     </div>
 </template>
 
 <script>
 import { eventBus } from "../main";
 import SimpleButton from "./SimpleButton";
+import SimpleSelect from "./SimpleSelect";
 
 export default {
     name: "wonder-selection-form",
@@ -16,18 +18,19 @@ export default {
     },
     mounted() {
         eventBus.$on("select-random-wonder", () => this.selectRandomWonder());
+        eventBus.$on("select-wonder", (index) => this.getWonder(index));
     },
     methods: {
         selectRandomWonder: function() {
             return this.getWonder(Math.random(0, wonders.length + 1));
         },
-
         getWonder: function(position) {
             return this.wonders[position];
         }
     },
     components: {
         "simple-button": SimpleButton,
+        "simple-select": SimpleSelect
     }
 }
 </script>
