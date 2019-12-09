3414c99afe89a25804306d7bc5bc34b76730f8ff
Cameron <cameron.beckit@gmail.com>
2019-12-09 14:50:24 +0000

    styles

--- client/src/components/WorldMap.vue
+++ client/src/components/WorldMap.vue
@@ -19,11 +19,13 @@ export default {
 
             let marker = WE.marker([this.wonder.details.latitude, this.wonder.details.longitude]).addTo(earth);
             marker.bindPopup(`<img src="${this.wonder.details.image}" class="img-fluid"/>
-            ${this.wonder.details.name}`,
+            <p class="popup-information-text"> Name: ${this.wonder.details.name}</p>
+            <p class="popup-information-text"> Location: ${this.wonder.details.location}</p>
+            <p class="popup-information-text"> Year Built: ${this.wonder.details.yearBuilt}<p>`,
                 {maxWidth: 300, maxHeight: 300, closeButton: true})
                 .openPopup();
 
-        earth.setView([this.wonder.details.latitude, this.wonder.details.longitude], 4);
+        earth.setView([this.wonder.details.latitude + 5 , this.wonder.details.longitude + 3], 4);
       }
     }
 }
@@ -33,4 +35,20 @@ export default {
 #earth_div{
   top: 0; right: 0; bottom: 0; left: 0;
   background-color: #000; position: absolute !important;}
+
+.popup-information-text{
+margin: 0px !important;
+margin-block-start: 0;
+margin-block-end: 0;
+margin-inline-start: 0;
+margin-inline-end: 0;
+
+font-size: 14px;
+color: #000;
+text-align: left;
+}
+.img-fluid{
+  margin-bottom: 5px;
+}
+
 </style>
