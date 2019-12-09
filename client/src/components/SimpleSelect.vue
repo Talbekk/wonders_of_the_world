9d4916097744e456f1914acb8e36f5080363016f
EismanW <pl.amosso@gmail.com>
2019-12-09 10:57:05 +0000

    added simple select

---
+++ client/src/components/SimpleSelect.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+       <select @change="onWonderSelection" required>
+        <option value="0" disabled selected>Choose a wonder</option>
+        <option v-for="(singleData, index) in data" :key="index" :value="index">{{singleData.name}}</option>
+        </select> 
+    </div>
+</template>
+
+<script>
+import { eventBus } from "../main";
+
+export default {
+    name: "simple-select",
+    props: {
+        eventChannel: String,
+        data: Array
+    },
+    methods: {
+        onWonderSelection: function(e) {
+            eventBus.$emit(eventChannel, e.target.value);
+        }
+    },
+}
+</script>
+
+<style lang="css" scoped>
+    
+</style>
