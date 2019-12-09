30d806c612612a3ec717a5eaf91d5d1ad745af26
Talbekk <mt.campbell@outlook.com>
2019-12-09 14:14:16 +0000

    adds connection to conversation

--- client/src/components/Conversation.vue
+++ client/src/components/Conversation.vue
@@ -1,13 +1,24 @@
 <template lang="html">
 
-<div class="">
-
+<div id="">
+<host></host>
+<traveller></traveller>
 </div>
 
 </template>
 
 <script>
+
+import Traveller from './traveller.vue';
+import Host from './host.vue';
+
 export default {
+  name: "conversation",
+  props: ['wonder'],
+    components: {
+      "host": Host,
+      "traveller": Traveller
+    }
 
 }
 
