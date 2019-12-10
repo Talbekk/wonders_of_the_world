b5595a1959dacd1a3e4aac5d3dcadfb437b242d2
Talbekk <mt.campbell@outlook.com>
2019-12-10 15:30:47 +0000

    "updated exit button"

--- client/src/components/Conversation.vue
+++ client/src/components/Conversation.vue
@@ -6,14 +6,10 @@
 
       <conversation-box :message="conversation.traveller" position="left" speech="left_speech_bubble" image="./conversation/traveller.png"></conversation-box>
     </div>
-
-
-
-    <div id="conversation-closure" class="conversation-popup" v-if="!conversation && conversationIndex > 0">
-      <p>What do you prefer to do now?</p>
-      <simple-button message="See more details" eventChannel="select-details"></simple-button>
-      <simple-button message="Go to homepage" eventChannel="select-homepage"></simple-button>
-    </div>
+      <!-- <div id="conversation-closure" class="conversation-popup" v-if="!conversation && conversationIndex > 0"> -->
+      <!-- <p>What do you prefer to do now?</p> -->
+      <simple-button v-if="!conversation && conversationIndex > 0" id="left-button" message="See more details" eventChannel="select-details"></simple-button>
+      <simple-button v-if="!conversation && conversationIndex > 0" id="right-button" message="X" eventChannel="select-homepage"></simple-button>
   </div>
 </template>
 
@@ -53,8 +49,29 @@ export default {
 <style lang="css" scoped>
 
 #conversation{
-  color: black;
-  background-color: white;
+  /* color: black;
+  background-color: white; */
   /* position: relative;} */
+}
+
+
+#left-button {
+  position: relative;
+  bottom: 280px;
+  left: 580px;
+  margin: 1em;
+  padding: 2em;
+  border: 2px solid #669999;
+  border-radius: 30%;
+}
+
+#right-button {
+  position: relative;
+  bottom: 280px;
+  right: 700px;
+  margin: 1em;
+  padding: 2em;
+  border: 2px solid #669999;
+  border-radius: 30%;
 }
 </style>
--- client/src/components/WonderPage.vue
+++ client/src/components/WonderPage.vue
@@ -34,6 +34,7 @@ export default {
   left: 50%;
   font-size: 15px;
   color: white;
+  /* color: white; */
   -webkit-transform: translate(-50%,-50%);
   transform: translate(-50%,-50%);
   -ms-transform: translate(-50%,-50);
