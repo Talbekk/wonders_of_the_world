4d8cb89976beea13c39f3d3d99c488effd00598a
EismanW <pl.amosso@gmail.com>
2019-12-11 15:28:06 +0000

    Styled more details page

--- client/src/components/MoreDetail.vue
+++ client/src/components/MoreDetail.vue
@@ -1,31 +1,42 @@
 <template lang="html">
   <div class="container">
-    <div class="row">
-      <div class="col">
-        <h1 class="details-head">{{wonder.details.name}}</h1>
-      </div>
+    <div class="row separation">
       <div class="col">
         <simple-button  class="home-button" eventChannel="select-homepage" message="Home"></simple-button>
       </div>
     </div>
-    <div class="row">
+    <div class="row separation">
       <div class="col">
-        <img  class="detail-image" :src="wonder.details.image">
-        <p class="first-elm">Height: {{wonder.details.height}}</p>
-        <p>Location: {{wonder.details.location}}</p>
-        <p>Year Built: {{wonder.details.yearBuilt}}</p>
-        <p>Purpose: {{wonder.details.purpose}}</p>
+        <h1 class="details-head">{{wonder.details.name}}</h1>
       </div>
-      <div class="col">
-      <p>{{wonder.details.description}}</p>
-      <p>{{wonder.details.description2}}</p>
-      <p>{{wonder.details.description3}}</p>
+    </div>
+    <div class="row separation center area">
+      <!-- <div class="col-sm-6"> -->
+        <img  class="img-fluid full-cover" :src="wonder.details.image">
+    </div>
+    <div class="row separation center area">
+        <div class="col-sm-3">
+          <strong>Height:</strong> {{wonder.details.height}}
+        </div>
+        <div class="col-sm-3">
+          <strong>Year Built:</strong> {{wonder.details.yearBuilt}}
+        </div>
+        <div class="col-sm-3">
+          <strong>Location:</strong> {{wonder.details.location}}
+        </div>
+        <div class="col-sm-3">
+          <strong>Purpose:</strong> {{wonder.details.purpose}}
+        </div>
+      </div>
+      <div class="row separation center area">
+        <p>{{wonder.details.description}}</p>
+        <p>{{wonder.details.description2}}</p>
+        <p>{{wonder.details.description3}}</p>
+      </div>
+    <div  id="last-image" class="row separation center">
+      <img class="gallery-image img-fluid full-cover" v-for='(image, index) in wonder.details.imageGallery' :key="index" :src="image">
     </div>
   </div>
-  <div  id="last-image" class="row">
-    <img class="gallery-image" v-for='(image, index) in wonder.details.imageGallery' :key="index" :src="image">
-  </div>
-</div>
 </template>
 
 <script>
@@ -49,8 +60,23 @@ export default {
 </script>
 
 <style lang="css" scoped>
-.detail-image{
-  height: 600px;
+.area {
+  margin: 0px 10px;
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+  width: auto; 
+  text-align: center;
+}
+
+.separation {
+  padding: 10px 0px;
+}
+
+.container {
+  background-color: white;
 }
 
 .details-head{
@@ -62,7 +88,13 @@ export default {
 }
 
 .gallery-image{
-  max-width: 25%;
+  width: 250px;
+  height: 250px;
+  margin: 10px;
+}
+
+.full-cover{
+  object-fit: cover;
 }
 
 .first-elm{
