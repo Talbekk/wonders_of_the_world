cc0ef189518effb6a237f69ebeff5a701288424e
Cameron <cameron.beckit@gmail.com>
2019-12-11 15:52:45 +0000

    adds quiz.vue

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home'
+import Quiz from '../views/Quiz'
 
 Vue.use(Router)
 
@@ -9,15 +10,12 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
-  }
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  },
+  {
+    path: '/quiz',
+    name: 'quiz',
+    component: Quiz
+  },
 ]
 const router = new Router({
   routes
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,7 +4,10 @@
     <!-- <user-form v-if="!username"></user-form> -->
     <wonder-selection-form id="right-form" :wonders ='wonders'></wonder-selection-form>
     <!-- <wonder-page v-if="map" :wonders="[selectedWonder]" :username="username"></wonder-page> -->
-    <!-- <button id="left-form" @click="onPlayQuizClick">Test your knowledge</button> -->
+    <div class="quiz-button">
+      <router-link to="/quiz">Test Your knowledge</router-link>
+    </div>
+    <!-- <button id="left-form" v-if="homepage" @click="onPlayQuizClick">Test your knowledge</button> -->
     <!-- <graphic-quiz :questions="questions" v-if="quiz"></graphic-quiz> -->
     <!-- <more-detail :wonder="selectedWonder" v-if="details"></more-detail> -->
     <conversation-box :message="hostMessage" position="right" speech="right_speech_bubble"
@@ -44,9 +47,36 @@ export default {
     hostMessage: function() {
       return `Hello ${this.username}, welcome to wonder of the worlds, what do you prefer to do?`
     }
-  }
+  },
+  methods: {
+    onPlayQuizClick: function() {
+      this.enableSection("quiz")
+    },
+}
 }
 </script>
 
 <style lang="css" scoped>
+ .quiz-button{
+   height: 200px;
+   width: 200px;
+   z-index: 100000;
+   position: absolute;
+   top: 20%;
+   left: 10%;
+   font-size: 15px;
+   color: white;
+   -webkit-transform: translate(-50%,-50%);
+   transform: translate(-50%,-50%);
+   -ms-transform: translate(-50%,-50);
+   padding-top: 100px;
+   color: black;
+   background-color: white;
+   margin: 1em;
+   padding: 2em;
+   max-width: 20%;
+   border: 2px solid #669999;
+   border-radius: 30%;
+   text-decoration: none;
+ }
 </style>
