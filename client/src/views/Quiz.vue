6b4682710e505bd7375dd6c55791ba3b56692364
EismanW <pl.amosso@gmail.com>
2019-12-12 11:21:16 +0000

    Removed quiz duplicated component

--- client/src/views/Quiz.vue
+++ client/src/views/Quiz.vue
@@ -1,74 +1,123 @@
-<template lang="html">
-  <div>
-    <graphic-quiz :questions="questions"></graphic-quiz>
-  </div>
-</template>
+<template>
+    <div class="question-area" v-if="currentQuestion">
+    <router-link class="top-left" to="/" tag="button">Go Home</router-link>
+      <graphic-quiz-question :question="currentQuestion.question"></graphic-quiz-question>
+        <div class="answer-area" v-if="showAnswer">
+            <graphic-quiz-answer
+            v-for="(answer, index) in currentQuestion.answers" :key="index"
+            @onSelectedImage="onSelectedImage"
+            :answer="answer">
+            </graphic-quiz-answer>
+        </div>
+        <div class="solution-area" v-if="showSolution">
+            <p>{{ solution.result | resultMessage }}</p>
+            <img :src="solution.result | resultImage"></img>
+            <router-link :to="{name: 'home'}">
+              <button>Home</button>
+            </router-link>
+            <button @click="onSelectedPlay">{{solution.button}}</button>
 
+        </div>
+    </div>
+</template>
 <script>
-import GraphicQuiz from "../components/GraphicQuiz";
 import GraphicQuizAnswer from "../components/GraphicQuizAnswer";
 import GraphicQuizQuestion from "../components/GraphicQuizQuestion";
 import SimpleButton from "../components/SimpleButton";
-
 import App from '../App.vue';
 
 export default {
-  name: 'quiz',
-  props: {
-      questions: Array
-  },
-  components: {
-    "graphic-quiz": GraphicQuiz,
-    "app": App,
-    "graphic-quiz-question": GraphicQuizQuestion,
-    "graphic-quiz-answer": GraphicQuizAnswer,
-    "simple-button": SimpleButton
-  },
-  methods: {
-    onPlayQuizClick: function() {
-      this.enableSection("quiz")
-  },
-  // watch: {
-  //   questions: function(x, y) {
-  //     if (x.length) {
-  //       this.enableSection
-  //     }
-  //   }
-  // }
-},
-data() {
-    return {
-        currentQuestionIndex: 0,
-        showAnswer: true,
-        showSolution: false,
-        solution: {
-            button: "Play Again",
-            result: false
-        }
-    }
-},
-computed: {
-    currentQuestion: function() {
-        return this.questions[this.currentQuestionIndex];
-    }
-},
-methods: {
-    onSelectedImage: function(result) {
-        this.solution.result = result;
-        this.showAnswer = false;
-        this.showSolution = true;
+    name: 'quiz',
+    props: {
+        questions: Array
     },
-    onSelectedPlay: function() {
-        // if (this.currentQuestionIndex === (this.questions.length - 1)) {
-        this.currentQuestionIndex++;
-        if (!this.questions[this.currentQuestionIndex]) {
-            this.currentQuestionIndex = 0;
+    data() {
+        return {
+            currentQuestionIndex: 0,
+            showAnswer: true,
+            showSolution: false,
+            solution: {
+                button: "Next",
+                result: false
+            }
         }
-        this.showAnswer = true;
-        this.showSolution = false;
-    }
-},
+    },
+    computed: {
+        currentQuestion: function() {
+            return this.questions[this.currentQuestionIndex];
+        }
+    },
+    methods: {
+        onSelectedImage: function(result) {
+            this.solution.result = result;
+            this.showAnswer = false;
+            this.showSolution = true;
+        },
+        onSelectedPlay: function() {
+            this.currentQuestionIndex++;
+            if (!this.questions[this.currentQuestionIndex]) {
+                this.currentQuestionIndex = 0;
+            }
+            this.showAnswer = true;
+            this.showSolution = false;
+        },
+    },
+    filters: {
+        resultMessage: function(value) {
+            return value ? "Nicely done! You are right" : "Wrong answer!"
+
+        },
+        resultImage: function(value){
+            return value ? "quiz/clapping.gif": "quiz/crying_bear.gif"
+        }
+    },
+    components: {
+        "graphic-quiz-question": GraphicQuizQuestion,
+        "graphic-quiz-answer": GraphicQuizAnswer,
+        "simple-button": SimpleButton
+    },
 }
 </script>
 <style lang="css" scoped>
+.answer-area {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        width: 100%;
+        max-width: 650px;
+        margin: 0 auto;
+        background-color: rgba(175, 220, 103, 0.81);
+    }
+    .solution-area {
+        padding: 20px;
+        background-color: #71d0e9;
+        font-size: 20px;
+    }
+    Button {
+      background-color: #44c767;
+      border-radius: 28px;
+      border: 1px solid #18ab29;
+      display: inline-block;
+      cursor: pointer;
+      color: #ffffff;
+      /* font-family: Arial; */
+      font-size: 20px;
+      padding: 16px 31px;
+      text-decoration: none;
+      text-shadow: 0px 1px 0px #2f6627;
+      margin: 10px;
+    }
+    Button:hover {
+    	background-color:#5cbf2a;
+    }
+    Button:active {
+    	position:relative;
+    	top:1px;
+    }
+    img {
+      display: flex;
+      margin: auto;
+      height: 150px;
+      width: auto;
+    }
 </style>
