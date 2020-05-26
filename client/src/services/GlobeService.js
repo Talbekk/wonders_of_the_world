ade9c997953e2e366b0a2112d39cb6d0a220d29c
Talbekk <mt.campbell@outlook.com>
2020-05-26 19:24:54 +0100

    chnages the res to text

--- client/src/services/GlobeService.js
+++ client/src/services/GlobeService.js
@@ -4,10 +4,12 @@ const quizURL = '/api/quiz/'
 export default {
   getWonders(){
     return fetch(wonderURL)
-    .then(res => res.json())
+    .then(res => res.text())
+    .then(text => console.log("wonders", text));
   },
   getQuiz(){
     return fetch(quizURL)
-    .then(res => res.json())
+    .then(res => res.text())
+    .then(text => console.log("quiz", text));
   },
 }
