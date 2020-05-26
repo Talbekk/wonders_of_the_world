73557b042c0d8b6d0779429dc28b678633443816
Talbekk <mt.campbell@outlook.com>
2020-05-26 15:04:02 +0100

    moves some things around

--- server/server.js
+++ server/server.js
@@ -8,17 +8,6 @@ const MongoClient = require('mongodb').MongoClient;
 app.use(bodyParser.json());
 app.use(cors());
 
-//Handle Production 
-if (process.env.NODE_ENV === 'production'){
-
-  //Static Folder
-  app.use(express.static(__dirname + '/public'));
-
-  //Handle single page application
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-  
-}
-
 // 'mongodb://mike123:Thejacket2@seven-wonders-shard-00-00-2wizi.mongodb.net:27017,seven-wonders-shard-00-01-2wizi.mongodb.net:27017,seven-wonders-shard-00-02-2wizi.mongodb.net:27017/test?ssl=true&replicaSet=seven-wonders-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 MongoClient.connect('mongodb://mike123:Thejacket2@seven-wonders-shard-00-00-2wizi.mongodb.net:27017,seven-wonders-shard-00-01-2wizi.mongodb.net:27017,seven-wonders-shard-00-02-2wizi.mongodb.net:27017/test?ssl=true&replicaSet=seven-wonders-shard-0&authSource=admin&retryWrites=true&w=majority',
@@ -38,6 +27,17 @@ MongoClient.connect('mongodb://mike123:Thejacket2@seven-wonders-shard-00-00-2wiz
 .catch((err) => {
   console.error(err);
 })
+
+//Handle Production 
+if (process.env.NODE_ENV === 'production'){
+
+  //Static Folder
+  app.use(express.static(__dirname + '/public'));
+
+  //Handle single page application
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  
+}
 
 const port = process.env.PORT || 5000;
 
