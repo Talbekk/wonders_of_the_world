ac4ffd6287386fbc8dd23cb80e53e67ccad714c6
Talbekk <mt.campbell@outlook.com>
2020-05-26 19:01:45 +0100

    adds condtional to index again

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 MongoClient.connect('mongodb://mike123:Thejacket2@seven-wonders-shard-00-00-2wizi.mongodb.net:27017,seven-wonders-shard-00-01-2wizi.mongodb.net:27017,seven-wonders-shard-00-02-2wizi.mongodb.net:27017/test?ssl=true&replicaSet=seven-wonders-shard-0&authSource=admin&retryWrites=true&w=majority',
 { useNewUrlParser: true, useUnifiedTopology: true  })
 .then((client) => {
-  console.log("inital connection", client);
+  console.log("inital connection", client); 
   const db = client.db('wonders_of_the_world');
   console.log("wonders", client.db('wonders_of_the_world'));
   const wondersCollection = db.collection('wonders');
@@ -28,7 +28,14 @@ MongoClient.connect('mongodb://mike123:Thejacket2@seven-wonders-shard-00-00-2wiz
   console.error(err);
 })
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
 
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 const port = process.env.PORT || 5000;
 
